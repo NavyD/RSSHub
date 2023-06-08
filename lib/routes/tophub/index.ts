0d377582a0434616ca589a85da09fd5d6cf188f3
navyd <dhjnavyd@gmail.com>
2023-06-08 15:30:26 +0800

    chore: Update douyin search direct link in tophub

--- lib/routes/tophub/index.ts
+++ lib/routes/tophub/index.ts
@@ -47,21 +47,22 @@ async function handler(ctx) {
     const title = $('div.Xc-ec-L.b-L').text().trim();
 
     const out = $('div.Zd-p-Sc > div:nth-child(1) tr')
-        .toArray()
-        .map((e) => {
-            const info = {
-                title: $(e).find('td.al a').text(),
-                link: $(e).find('td.al a').attr('href'),
-            };
-            if (id === 'K7GdaMgdQy') {
-                // 抖音
-                info.link = 'https://www.douyin.com/search/' + encodeURIComponent(info.title);
-            } else if (id === 'Om4ejxvxEN') {
-                // 贴吧
-                info.link = 'https://tieba.baidu.com/hottopic/browse/hottopic?topic_name=' + encodeURIComponent(info.title);
+        .map(function () {
+            const title = $(this).find('td.al a').text();
+
+            let link;
+            if (id === "K7GdaMgdQy") { // 抖音
+                link = 'https://www.douyin.com/search/' + encodeURIComponent(title);
+            } else {
+                link = $(this).find('td.al a').attr('href');
             }
-            return info;
-        });
+
+            return {
+                title,
+                link,
+            };
+        })
+        .get();
 
     return {
         title,
